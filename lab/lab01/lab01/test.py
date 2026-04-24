bbe5ae7a810ce261bee1ae6c7f6cabde51a0d5ee
BenzyYue <yue94281@gmail.com>
2026-04-24 10:33:28 +0900

    Add cs61a-selfsudy code

---
+++ lab/lab01/lab01/test.py
@@ -0,0 +1,34 @@
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    i = 0
+    while n > 0:
+        if n % 10 == 8:
+            i = i + 1
+            n = n // 10
+            if i == 2:
+                return True
+            else:
+                continue
+        else:
+            i = 0
+            n = n // 10
+    return False
+
+print(double_eights(8))
+print(double_eights(88))
+print(double_eights(2882))
+print(double_eights(880088))
